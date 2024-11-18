0f9c2ab84ec1ff3d0a1221079343aa4ec2a9e053
Nabin Shrestha <nabins724@gmail.com>
2024-11-19 09:48:43 +1100

    Production Test

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 // Connecting to MongoDB Database
