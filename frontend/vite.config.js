18a7714194af592e8488286f9e05e4ef3b99d57e
Nabin Shrestha <nabins724@gmail.com>
2024-11-21 09:43:18 +1100

    Route changed to test AWS Amplify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy:{
       "/api":{
-        target: "http://localhost:5000",
+        target: "https://main.dau1331g1efhn.amplifyapp.com",
         changeOrigin: true,
       }
     }
