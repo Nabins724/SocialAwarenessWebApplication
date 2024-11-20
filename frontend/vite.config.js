24f3ab6a884de5ca4fba8d12d8d20daa830cd12b
Nabin Shrestha <nabins724@gmail.com>
2024-11-21 09:53:02 +1100

    Revert Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server:{
     port: 3000,
     proxy:{
-      "":{
-        target: "https://main.dau1331g1efhn.amplifyapp.com",
+      "/api":{
+        target: "http://localhost:5000",
         changeOrigin: true,
       }
     }
