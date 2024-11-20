4b93e3baea104593136f9b2f61a4bd214b850ed0
Nabin Shrestha <nabins724@gmail.com>
2024-11-20 16:34:52 +1100

    Updating bakend server port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy:{
       "/api":{
-        target: "http://localhost:5000",
+        target: "https://13.210.27.141.nip.io",
         changeOrigin: true,
       }
     }
