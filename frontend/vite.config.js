1ce46910c4f0e5f24dcf9bf823d74214ca0f9754
Nabin Shrestha <nabins724@gmail.com>
2024-11-21 09:47:21 +1100

    Removed api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     port: 3000,
     proxy:{
-      "/api":{
+      "":{
         target: "https://main.dau1331g1efhn.amplifyapp.com",
         changeOrigin: true,
       }
